6f1cb0050af55a85879f56b1e43510610a33d0bd
Mark S <markschwanethal@gmail.com>
2026-05-19 13:34:43 +1000

    feat: implement get arnie function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,11 +1,24 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 /**
  * @param {GetArnieQuotesInput} urls
  * @returns {ArnieQuoteResponse}
  */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
+  const response = await Promise.all(
+    urls.map(async (url) => {
+      const res = await httpGet(url);
+
+      return {
+        status: res.status,
+        message: JSON.parse(res.body).message,
+      };
+    }),
+  );
+
+  return response.map(({ status, message }) => ({
+    [status === 200 ? "Arnie Quote" : "FAILURE"]: message,
+  }));
 };
 
 module.exports = {
